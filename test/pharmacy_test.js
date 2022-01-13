8a049864ac621f595cecebe899a5187d247673c6
singhuacai <singhuacai@gmail.com>
2022-01-13 21:47:49 +0800

    add the test of get pharmacies by mask price fn

--- test/pharmacy_test.js
+++ test/pharmacy_test.js
@@ -57,4 +57,118 @@ describe("pharmacy", function () {
       expect(res.body.error).to.equal("you key the wrong time!");
     });
   });
+  context("get pharmacy masks", function () {
+    it("with right parameter, sort by mask_price (desc)", async () => {
+      const res = await requester.get(
+        "/pharmacy/masksList?pharmacy=Cash%20Saver%20Pharmacy&sortby=mask_price&desc=true"
+      );
+      const data = res.body.masksList;
+      const expected = [
+        {
+          name: "AniMask (green) (10 per pack)",
+          price: "49.21",
+        },
+        {
+          name: "MaskT (black) (10 per pack)",
+          price: "14.90",
+        },
+        {
+          name: "Free to Roam (black) (3 per pack)",
+          price: "13.83",
+        },
+      ];
+      expect(data).to.deep.equal(expected);
+    });
+
+    it("with right parameter, sort by mask_price", async () => {
+      const res = await requester.get(
+        "/pharmacy/masksList?pharmacy=Cash%20Saver%20Pharmacy&sortby=mask_price&desc=false"
+      );
+      const data = res.body.masksList;
+      const expected = [
+        {
+          name: "Free to Roam (black) (3 per pack)",
+          price: "13.83",
+        },
+        {
+          name: "MaskT (black) (10 per pack)",
+          price: "14.90",
+        },
+        {
+          name: "AniMask (green) (10 per pack)",
+          price: "49.21",
+        },
+      ];
+      expect(data).to.deep.equal(expected);
+    });
+
+    it("with right parameter, sort by mask_name (desc)", async () => {
+      const res = await requester.get(
+        "/pharmacy/masksList?pharmacy=Cash%20Saver%20Pharmacy&sortby=mask_name&desc=true"
+      );
+      const data = res.body.masksList;
+      const expected = [
+        {
+          name: "MaskT (black) (10 per pack)",
+          price: "14.90",
+        },
+        {
+          name: "Free to Roam (black) (3 per pack)",
+          price: "13.83",
+        },
+        {
+          name: "AniMask (green) (10 per pack)",
+          price: "49.21",
+        },
+      ];
+      expect(data).to.deep.equal(expected);
+    });
+
+    it("with right parameter, sort by mask_name", async () => {
+      const res = await requester.get(
+        "/pharmacy/masksList?pharmacy=Cash%20Saver%20Pharmacy&sortby=mask_name&desc=false"
+      );
+      const data = res.body.masksList;
+      const expected = [
+        {
+          name: "AniMask (green) (10 per pack)",
+          price: "49.21",
+        },
+        {
+          name: "Free to Roam (black) (3 per pack)",
+          price: "13.83",
+        },
+
+        {
+          name: "MaskT (black) (10 per pack)",
+          price: "14.90",
+        },
+      ];
+      expect(data).to.deep.equal(expected);
+    });
+
+    it("with wrong parameter(sort by)", async () => {
+      const res = await requester.get(
+        "/pharmacy/masksList?pharmacy=Cash%20Saver%20Pharmacy&sortby=name&desc=false"
+      );
+      expect(res.statusCode).to.equal(400);
+      expect(res.body.error).to.equal("you key the wrong sortby!");
+    });
+
+    it("with wrong parameter(desc)", async () => {
+      const res = await requester.get(
+        "/pharmacy/masksList?pharmacy=Cash%20Saver%20Pharmacy&sortby=mask_name&desc=desc"
+      );
+      expect(res.statusCode).to.equal(400);
+      expect(res.body.error).to.equal("you key the wrong desc status!");
+    });
+
+    it("lack parameter", async () => {
+      const res = await requester.get("/pharmacy/masksList");
+      expect(res.statusCode).to.equal(400);
+      expect(res.body.error).to.equal(
+        "pharmacy, sortby and desc parameters are required!"
+      );
+    });
+  });
 });
